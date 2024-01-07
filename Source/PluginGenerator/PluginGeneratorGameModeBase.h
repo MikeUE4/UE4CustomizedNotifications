0e6943ba3f1c77cd98cdb1b43dbe88b333663c47
RearAndroidUE4 <37648290+MikeUE4@users.noreply.github.com>
2024-01-08 02:02:03 +0530

    Add files via upload

---
+++ Source/PluginGenerator/PluginGeneratorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PluginGeneratorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLUGINGENERATOR_API APluginGeneratorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
